a017d6114c603b470f6fbe53ccc05b9043b85aa7
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-08-25 11:14:05 -0500

    create a form to experiment with GET and POST METHODS
    --NOTE that POST does not put entered values into URL
      GET does push values into URL

---
+++ public/my_first_form.php
@@ -0,0 +1,24 @@
+<?php 
+
+	var_dump($_POST);
+	var_dump($_GET);
+
+	?>
+
+	<html>
+	<head>
+			<title>My First Form</title>
+	</head>
+	<body>
+
+		
+		<form method="GET">
+			<p><input type="text" name="first_name" id="first_name" /></p>
+			<input type ="submit"/>
+		</form>
+
+
+	</body>
+	</html>
+
+
