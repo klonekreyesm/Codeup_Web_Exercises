7f050bec7b96335915003ab702d23b4d557770b2
klonekreyesm <mary.reyes00@gmail.com>
2014-09-29 13:26:45 -0500

    Using Jquery craft selectors to change font size and highlight p, li elements

--- public/js/jquery_exercises.js
+++ public/js/jquery_exercises.js
@@ -1,4 +1,5 @@
 $('document').ready(function() {
-            $('.codeup').css('border-style', 'solid');
-            $('#list').css('border-color', 'red');
+            // $('.codeup').css('border-style', 'solid');
+            $('li').css('font-size', '20px')
+            $('h1, p, li').css('background-color', 'yellow');
         });
